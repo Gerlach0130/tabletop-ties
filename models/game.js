8d32787c8d187c17891ba46bd28a249bd9bccd88
Jess Skelton <jessskelton888@gmail.com>
2024-02-01 19:20:59 -0600

    Completed models/game.js

--- models/game.js
+++ models/game.js
@@ -9,8 +9,42 @@ class Game extends Model {};
 Game.init(
     {
         id: {
-
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        genre: {
+            type: DataTypes.STRING
+        },
+        player_count: {
+            type: DataTypes.INTEGER
+        },
+        avg_play_time: {
+            type: DataTypes.INTEGER
+        },
+        description: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
         }
+    },
+    {
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post'
     }
 );
 
