eded73656446d3f27a8592984115355104a4fbbd
Michael Gerlach <mgerlach0130@gmail.com>
2024-02-01 19:11:59 -0600

    added handlebars directories

--- public/js/login.js
+++ public/js/login.js
@@ -35,6 +35,6 @@ const signupFormHandler = async (event) => {
       }
     }
 };
-  
+
 document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
 document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
