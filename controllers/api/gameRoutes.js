fdd4386b0643a883748652b9e967d396e7d01232
Jess Skelton <jessskelton888@gmail.com>
2024-02-07 22:10:36 -0600

    Debugged homeRoutes.js --- Added search routes to both gameRoutes.js and eventRoutes.js

--- controllers/api/gameRoutes.js
+++ controllers/api/gameRoutes.js
@@ -1,6 +1,6 @@
 // Imports
 const router = require('express').Router();
-const { Game, UsersGames } = require('../../models');
+const { User, Game, UsersGames } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 // GET route to fetch all games
@@ -14,20 +14,58 @@ router.get('/', async (req, res) => {
 });
 
 // GET route to fetch specific game
-
-// GET route to fetch games by genre
-router.get('/genre/:genre', async (req, res) => {
+router.get('/:id', async (req, res) => {
     try {
-        const gameByGenre = await Game.findAll({
-            where: { genre: req.params.genre }
+        const gameData = await Game.findByPk(req.params.id, {
+            include: [
+                { model: User, attributes: ['name'] },
+                { model: Game, include: [{ model: User, attributes: ['name'] }] }
+            ]
         });
-        res.status(200).json(gameByGenre);
-    } catch (error) {
-        res.status(500).json(error);
+
+        if (!gameData) {
+            res.status(404).json({ message: 'No game found with this id!' });
+            return;
+        }
+
+        res.status(200).json(gameData);
+    } catch (err) {
+        res.status(500).json(err);
     }
 });
 
-// POST route for user to add a game to profile interests **********POSSIBLY CHANGE /ADD TO JUST / IF ERRORS
+// GET route to search by genre
+router.get('/genre', async (req, res) => {
+    try {
+        const gamesByGenre = await Game.findAll({
+            include: [{
+                model: Genre, 
+                where: { name: req.query.genre }
+            }]
+            // Possibly add other relevant associations and attributes
+        });
+        res.status(200).json(gamesByGenre);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// GET route to search by game title
+router.get('/title', async (req, res) => {
+    try {
+        const gameData = await Game.findAll({
+            where: {
+                title: sequelize.where(sequelize.fn('LOWER', sequelize.col('title')), 'LIKE', `%${req.query.title.toLowerCase()}%`)
+            }
+            // Possibly add other relevant associations and attributes
+        });
+        res.status(200).json(gameData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// POST route for user to add a game to profile interests **********POSSIBLY CHANGE '/ADD' TO JUST '/' IF ERRORS
 router.post('/add', withAuth, async (req, res) => {
     try {
         const newGameInterest = await UsersGames.create({
